a28edf88b2723e4e627691eef8e92c340fccf4bc
Victor Zoré <victorzore@MacBook-Pro-de-Victor.local>
2025-06-24 16:48:39 -0300

    atualizacao portfolio

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Footer.css';
-
-const Footer = () => (
-  <footer className="footer-section">
-    <p>© {new Date().getFullYear()} - Data Scientist Portfolio</p>
-  </footer>
-);
-export default Footer;
